26750a6e1f727f2de354abc02558e40122af646c
Hugo Backlund <hugo.e.backlund@gmail.com>
2024-10-19 02:38:59 +0200

    proper naming convention for hidden modules

--- src/rotational_placement/__init__.py
+++ src/rotational_placement/__init__.py
@@ -3,6 +3,7 @@ from .experiment_class import Experiment
 from .load_config import load_config
 from .make_plot_dir import make_plot_dir
 from .make_data_dir import make_data_dir
+from .plot_density import plot_density
 
 
 __version__ = "0.1.6"
@@ -11,4 +12,5 @@ __all__ = [
     "load_config",
     "make_plot_dir",
     "make_data_dir",
+    "plot_density"
 ]
